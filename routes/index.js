f937aed251eb687a6a5be39fd25ad1e1c861922e
JY_DeskTop <3220ju@naver.com>
2020-09-03 00:08:23 +0900

    메인 페이지, 회원 가입 페이지 제작 - 메인페이지 추가할컨텐츠 고민

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,10 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('main', { title: '인프런 클론' });
+});
+router.get('/join', function(req, res, next) {
+  res.render('join', { title: '인프런 클론 - 회원가입' });
 });
 
 module.exports = router;
