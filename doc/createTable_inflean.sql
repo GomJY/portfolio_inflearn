f76de6d2b144a48a0de5bf2bb645e29e2b9c36fa
sourceTreeMac <3220ju@naver.com>
2020-09-15 11:55:13 +0900

    [프론트]강의 만들기, 메인 페이지 UI 번경

--- doc/createTable_inflean.sql
+++ doc/createTable_inflean.sql
@@ -1,3 +1,4 @@
+-- create database inflean;
 use inflean;
 CREATE TABLE `users` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
@@ -6,7 +7,7 @@ CREATE TABLE `users` (
   `nickname` varchar(50) NOT NULL,
   `authority` int(11) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 ;
 
 CREATE TABLE `lectures` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
@@ -19,7 +20,7 @@ CREATE TABLE `lectures` (
   PRIMARY KEY (`id`),
   KEY `user_lecture_idx` (`user_id`),
   CONSTRAINT `user_lecture` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 ;
 
 CREATE TABLE `sections` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
@@ -29,7 +30,7 @@ CREATE TABLE `sections` (
   PRIMARY KEY (`id`),
   KEY `lectures_sections_idx` (`lectures_id`),
   CONSTRAINT `lectures_sections` FOREIGN KEY (`lectures_id`) REFERENCES `lectures` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 ;
 
 CREATE TABLE `chapters` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
@@ -39,7 +40,7 @@ CREATE TABLE `chapters` (
   PRIMARY KEY (`id`),
   KEY `sections_chapters_idx` (`sections_id`),
   CONSTRAINT `sections_chapters` FOREIGN KEY (`sections_id`) REFERENCES `sections` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 ;
 
 CREATE TABLE `questions` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
@@ -51,7 +52,7 @@ CREATE TABLE `questions` (
   PRIMARY KEY (`id`),
   KEY `questions_lectureId_idx` (`lecture_id`),
   CONSTRAINT `questions_lectureId` FOREIGN KEY (`lecture_id`) REFERENCES `lectures` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 ;
 
 CREATE TABLE `questions_comments` (
   `question_id` int NOT NULL,
@@ -60,7 +61,7 @@ CREATE TABLE `questions_comments` (
   KEY `comment_questionId_idx` (`question_id`),
   CONSTRAINT `comment_commentId` FOREIGN KEY (`commentId`) REFERENCES `questions` (`id`),
   CONSTRAINT `comment_questionId` FOREIGN KEY (`question_id`) REFERENCES `questions` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 ;
 
 
 CREATE TABLE `tags` (
@@ -68,7 +69,7 @@ CREATE TABLE `tags` (
   `name` varchar(255) NOT NULL,
   `createdTime` datetime NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 ;
 
 
 CREATE TABLE `tags_and_lectures` (
@@ -78,7 +79,7 @@ CREATE TABLE `tags_and_lectures` (
   KEY `tags_idx` (`tags_id`),
   CONSTRAINT `lectures` FOREIGN KEY (`lectures_id`) REFERENCES `lectures` (`id`),
   CONSTRAINT `tags` FOREIGN KEY (`tags_id`) REFERENCES `tags` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 ;
 
 
 CREATE TABLE `likes` (
@@ -89,4 +90,4 @@ CREATE TABLE `likes` (
   KEY `lectures_idx` (`lectures_id`),
   CONSTRAINT `lectures_like` FOREIGN KEY (`lectures_id`) REFERENCES `lectures` (`id`),
   CONSTRAINT `users_like` FOREIGN KEY (`users_id`) REFERENCES `users` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 ;
